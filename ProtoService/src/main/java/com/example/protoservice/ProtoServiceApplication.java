a650d5a091b16b9284dd96b47a8cf3ad1679fc14
rococo4 <aalebedev_8@edu.hse.ru>
2024-06-15 20:00:27 +0300

    Add ProtoService

---
+++ ProtoService/src/main/java/com/example/protoservice/ProtoServiceApplication.java
@@ -0,0 +1,10 @@
+package com.example.protoservice;
+
+
+//public class ProtoServiceApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ProtoServiceApplication.class, args);
+//    }
+//
+//}
