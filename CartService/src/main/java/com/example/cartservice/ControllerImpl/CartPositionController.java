5261ba54be91bb33966af2024dfa0a9d937eeee6
rococo4 <aalebedev_8@edu.hse.ru>
2024-06-16 19:23:04 +0300

    Cart Controllers implementation

--- CartService/src/main/java/com/example/cartservice/ControllerImpl/CartPositionController.java
+++ CartService/src/main/java/com/example/cartservice/ControllerImpl/CartPositionController.java
@@ -1,34 +1,41 @@
 package com.example.cartservice.ControllerImpl;
 
+import com.example.cartservice.Services.CartPositionsService;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import org.example.cartservice.CartPositionControllerGrpc;
 import org.example.cartservice.CartService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
+@Component
+@RequiredArgsConstructor
 public class CartPositionController extends CartPositionControllerGrpc.CartPositionControllerImplBase {
+    private final CartPositionsService cartPositionsService;
     @Override
     public void createCartPosition(CartService.CartPositionRequest request,
                                    StreamObserver<CartService.CartPositionResponse> responseObserver) {
-
+        cartPositionsService.createCartPosition(request, responseObserver);
     }
 
 
     @Override
-    public void updateCartPosition(CartService.CartRequestWithId request,
+    public void updateCartPosition(CartService.CartPositionRequestWithId request,
                                    StreamObserver<CartService.CartPositionResponse> responseObserver) {
-
+        cartPositionsService.updateCartPosition(request, responseObserver);
     }
 
 
     @Override
     public void getCartPosition(CartService.CartPositionId request,
                                 StreamObserver<CartService.CartPositionResponse> responseObserver) {
-
+        cartPositionsService.getCartPosition(request, responseObserver);
     }
 
     @Override
     public void deleteCartPosition(CartService.CartPositionId request,
-                                   StreamObserver<CartService.CartPositionResponse> responseObserver) {
-
+                                   StreamObserver<com.google.protobuf.Empty> responseObserver) {
+        cartPositionsService.deleteCartPosition(request, responseObserver);
     }
 
 }
