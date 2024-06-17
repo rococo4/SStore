8921feeb76510ae592313f1a8dbc203d30477bbf
rococo4 <aalebedev_8@edu.hse.ru>
2024-06-17 20:42:05 +0300

    Add order service

---
+++ OrderService/src/test/java/com/sstore/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sstore.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
