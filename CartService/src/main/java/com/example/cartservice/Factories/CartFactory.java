b79358206b2731580c0bf7b7a14ee969430e7020
rococo4 <aalebedev_8@edu.hse.ru>
2024-06-17 23:14:23 +0300

    Add requests for user and sneaker

--- CartService/src/main/java/com/example/cartservice/Factories/CartFactory.java
+++ CartService/src/main/java/com/example/cartservice/Factories/CartFactory.java
@@ -2,8 +2,13 @@ package com.example.cartservice.Factories;
 
 import com.example.cartservice.store.Entities.CartEntity;
 import com.google.protobuf.util.Timestamps;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import lombok.RequiredArgsConstructor;
 import org.example.cartservice.CartService;
+import org.example.orchestratorservice.OrchestratorControllerGrpc;
+import org.example.userservice.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.google.protobuf.Timestamp;
 
@@ -12,6 +17,8 @@ import com.google.protobuf.Timestamp;
 @Component
 @RequiredArgsConstructor
 public class CartFactory {
+    @Value("${OrchestratorService.grpc.server.port}")
+    private int orchestratorPort;
     private final CartPositionFactory cartPositionFactory;
     public CartService.CartResponse makeCartResponse(CartEntity cartEntity) {
         return CartService.CartResponse.newBuilder()
@@ -24,8 +31,17 @@ public class CartFactory {
                 .setUser(getUserById(cartEntity.getUserId()))
                 .build();
     }
-    private CartService.UserResponse getUserById(Long userId) {
-        return null;
+    private UserService.UserResponse getUserById(Long userId) {
+        try {
+            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", orchestratorPort)
+                    .usePlaintext().build();
+            OrchestratorControllerGrpc.OrchestratorControllerBlockingStub blockingStub =
+                    OrchestratorControllerGrpc.newBlockingStub(channel);
+            UserService.UserId userIdClass = UserService.UserId.newBuilder().setUserId(userId).build();
+            return blockingStub.getUserById(userIdClass);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- CartService/src/main/java/com/example/cartservice/Factories/CartPositionFactory.java
+++ CartService/src/main/java/com/example/cartservice/Factories/CartPositionFactory.java
@@ -1,13 +1,18 @@
 package com.example.cartservice.Factories;
 
 import com.example.cartservice.store.Entities.CartPositionEntity;
-import jakarta.persistence.Column;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.example.cartservice.CartService;
+import org.example.orchestratorservice.OrchestratorControllerGrpc;
 import org.example.productservice.ProductService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartPositionFactory {
+    @Value("${OrchestratorService.grpc.server.port}")
+    private int orchestratorPort;
     public CartService.CartPositionResponse makeCartPositionResponse(CartPositionEntity cartPositionEntity) {
         return CartService.CartPositionResponse.newBuilder()
                 .setPositionId(cartPositionEntity.getId())
@@ -16,6 +21,17 @@ public class CartPositionFactory {
                 .build();
     }
     private ProductService.SneakerResponse getSneakerById(Long sneakerId) {
-        return null;
+        try {
+            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", orchestratorPort)
+                    .usePlaintext().build();
+            OrchestratorControllerGrpc.OrchestratorControllerBlockingStub blockingStub =
+                    OrchestratorControllerGrpc.newBlockingStub(channel);
+            ProductService.SneakerId sneakerIdClass = ProductService.SneakerId.newBuilder()
+                    .setSneakerId(sneakerId)
+                    .build();
+            return blockingStub.getSneakerById(sneakerIdClass);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
